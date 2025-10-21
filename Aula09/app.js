8c459b37937f0268ad022d32bfdadc6380c4e71f
Joel Santos <i3975@rn.senac.local>
2025-10-21 14:30:29 -0300

    feat: implement Categoria model and controller with CRUD operations

--- Aula09/app.js
+++ Aula09/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import 'dotenv/config';
 import autorRoutes from "./src/routes/autorRoutes.js";
+import categoriaRoutes from "./src/routes/categoriaRoutes.js";
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
 app.use("/autores", autorRoutes);
+app.use("/categorias", categoriaRoutes);
 
 app.listen(port, ()=>{
     console.log(`Servidor rodando em: http://localhost:${port}`);
