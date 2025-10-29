e17223fb796fada8eb5afd4ca1253b7ee18528ac
Joel Santos <i3975@rn.senac.local>
2025-10-29 14:52:38 -0300

    feat: Aula 29-10-25 Trabalhando com api viacep

--- Aula11/src/routes/usuarioRoutes.js
+++ Aula11/src/routes/usuarioRoutes.js
@@ -6,5 +6,7 @@ const router = express.Router();
 router.get("/", UsuarioController.listar);
 router.get("/:id", UsuarioController.buscarPorId);
 router.post("/", UsuarioController.criar);
+router.put("/:id", UsuarioController.atualizar);
+router.delete("/:id", UsuarioController.deletar);
 
 export default router;
